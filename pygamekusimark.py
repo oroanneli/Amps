a9231274ed8e580e9e1abea3bbc452b156276d7f
Anneli Oro <anneli.oro@gmail.com>
2023-03-16 22:39:10 +0200

    tegi koodi loogilisemaks veits

--- pygamekusimark.py
+++ pygamekusimark.py
@@ -2,10 +2,18 @@ import pygame
 import pygame as pg
 from random import randint
 
-size= width, height= (300, 500)
-x=width/2
-xk= randint(0,280)
-y=0
+#terve hunniks muutujaid
+size= width, height= (800, 500)
+#kujund
+a=30
+b=30
+#kiirus
+v= 5
+#kortinatid
+x_player=width/2
+y_player=height-b
+x_kast= randint(0,280)
+y_kast=0
 
 pg.init()
 
@@ -14,35 +22,30 @@ aken = pygame.display.set_mode(size)
 pg.display.set_caption("jah?")
 gameplay= True
 
-#kujund
-a=20
-b=20
-#kiirus
-v= 5
-
 #mängu loop
 while gameplay:
     pg.time.delay(10)
     for event in pg.event.get():
         if event.type== pg.QUIT:
             gameplay=False
+
     keys=pg.key.get_pressed()
-    if keys[pygame.K_LEFT] and x>0:
-        x-= v
-    if keys[pygame.K_RIGHT] and x<300-a:
-        x+= v
-    if y<500:
-        y+=5
+    if keys[pygame.K_LEFT] and x_player>0:
+        x_player-= v
+    if keys[pygame.K_RIGHT] and x_player<width-a:
+        x_player+= v
+    if y_kast<500:
+        y_kast+=5
     else:
-        y=50
-        xk=randint(0,280)
+        y_kast=0
+        x_kast=randint(0,280)
 
     #joonistame
     aken.fill((200,200,200))
-    #tegelintski
-    pg.draw.rect(aken, (100,150,150), (x, 480, a, b))
+    pg.draw.rect(aken, (100,150,150), (x_player, y_player, a, b))
 
-    pg.draw.rect(aken,(0, 255, 255), (xk, y, a, b))
+#tegelintski
+    pg.draw.rect(aken,(0, 255, 255), (x_kast, y_kast, a, b))
     pg.display.update()
 
 
