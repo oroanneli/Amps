4bc3a9b978d1d3f8f863f80672c3784453e98f6f
Anneli Oro <anneli.oro@gmail.com>
2023-03-16 23:53:38 +0200

    see tont kukub nüüd kiiremini alla + collision detection + score

--- pygamekusimark.py
+++ pygamekusimark.py
@@ -4,18 +4,23 @@ from random import randint
 
 #terve hunniks muutujaid
 size= width, height= (800, 500)
+skoor=0
+
+
 #kujund
 a=30
-b=30
+
 #kiirus
-v= 5
+v_player= 10
+v_kast=5
 #kortinatid
 x_player=width/2
-y_player=height-b
-x_kast= randint(0,280)
+y_player=height-a
+x_kast= randint(0,width)
 y_kast=0
 
 pg.init()
+pygame.font.init()
 
 #set up
 aken = pygame.display.set_mode(size)
@@ -29,23 +34,36 @@ while gameplay:
         if event.type== pg.QUIT:
             gameplay=False
 
+    #mängija liikumine
     keys=pg.key.get_pressed()
     if keys[pygame.K_LEFT] and x_player>0:
-        x_player-= v
+        x_player-= v_player
     if keys[pygame.K_RIGHT] and x_player<width-a:
-        x_player+= v
-    if y_kast<500:
-        y_kast+=5
-    else:
-        y_kast=0
-        x_kast=randint(0,280)
+        x_player+= v_player
 
-    #joonistame
+    if y_kast<500:
+        y_kast+=v_kast
+        v_kast+=0.001
+    else:
+        x_kast=randint(0,width-a)
+        y_kast=0
+    if y_player < (y_kast+a):
+        if ((x_player > x_kast and x_player < (x_kast + a)) or ((x_player + a) > x_kast and (x_player + a) < (x_kast + a))):
+            skoor+=1
+            x_kast=randint(0,width-a)
+            y_kast=0
+
+
+
+#joonistame
     aken.fill((200,200,200))
-    pg.draw.rect(aken, (100,150,150), (x_player, y_player, a, b))
+    pg.draw.rect(aken, (100,150,150), (x_player, y_player, a, a))
 
 #tegelintski
-    pg.draw.rect(aken,(0, 255, 255), (x_kast, y_kast, a, b))
+    pg.draw.rect(aken,(0, 255, 255), (x_kast, y_kast, a, a))
+    font = pygame.font.SysFont(None, 24)
+    img = font.render("skoor: " + str(skoor), True, "black")
+    aken.blit(img, (20, 20))
     pg.display.update()
 
 
