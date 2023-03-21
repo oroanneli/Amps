476297f760a2a1c0c889ce11d2be2e483c7565d6
Anneli Oro <anneli.oro@kool.nrg.edu.ee>
2023-03-21 19:11:11 +0200

    fixisin selle värvi jama, elud töötavad, +1 surmav sõõr

--- pygamekusimark.py
+++ pygamekusimark.py
@@ -4,20 +4,23 @@ from random import randint
 from random import choice
 
 #terve hunniks muutujaid
+lopp=False
 size= width, height= (800, 500)
 skoor=0
+elud=3
 #tüüp:(värv) sinine; kollane; roheline, roosa
-erinevad_kastid= {"normal":(190,250,255), "2xSkoor":(255,255,176), "0.5xK_Kiirus": (200, 255, 190), "2xK_Kiirus": (255, 192, 192)}
+erinevad_kastid= {"normal":(190,250,255), "2xSkoor":(255,255,176), "0.5xK_Kiirus": (200, 255, 190), "2xK_Kiirus": (255, 192, 192), "mürgine":(61,61,61), "+1elu": (175,130,250)}
 tüüp= choice(list(erinevad_kastid.keys()))
 värv=erinevad_kastid[tüüp]
 #kujund
 a=30
+tegelane=50
 #kiirus
 v_player= 10
 v_kast=5
 #kortinatid
 x_player=width/2
-y_player=height-a
+y_player=height-tegelane
 x_kast= randint(0,width)
 y_kast=0
 
@@ -40,50 +43,75 @@ while gameplay:
     keys=pg.key.get_pressed()
     if keys[pygame.K_LEFT] and x_player>0:
         x_player-= v_player
-    if keys[pygame.K_RIGHT] and x_player<width-a:
+    if keys[pygame.K_RIGHT] and x_player<width-tegelane:
         x_player+= v_player
 
     #kasti kukkumine
-    if y_kast<500:
+    if y_kast<550:
         y_kast+=v_kast
         v_kast+=0.001
+
     else:
         x_kast=randint(0,width-a)
         y_kast=0
+        if tüüp!="mürgine":
+            elud-=1
+        lopp = True
 
     #kontrolli kas kukkus pähe v ei
-    if y_player < (y_kast+a):
-        if ((x_player > x_kast and x_player < (x_kast + a)) or ((x_player + a) > x_kast and (x_player + a) < (x_kast + a))):
+    if y_player < (y_kast+tegelane):
+        if ((x_player > x_kast and x_player < (x_kast + tegelane)) or ((x_player + tegelane) > x_kast and (x_player + tegelane) < (x_kast + tegelane))):
+
+            #mässab skooriga
             if tüüp=="2xSkoor":
                 skoor+=2
+            elif tüüp=="2xK_Kiirus":
+                skoor += 3
+            elif tüüp=="0.5xK_Kiirus":
+                skoor-=1
             else:
                 skoor+=1
+
+            #muud efektid
             if tüüp =="0.5xK_Kiirus":
                 v_kast=v_kast*0.5
             elif tüüp =="2xK_Kiirus":
                 v_kast=v_kast*1.1
+            elif tüüp=="mürgine":
+                gameplay=False
+            elif tüüp=="+1elu":
+                elud+=1
+
 
             #reset
-            x_kast=randint(0,width-a)
+            x_kast=randint(0, width-a)
             y_kast=0
-        tüüp= choice(list(erinevad_kastid.keys()))
-        värv=erinevad_kastid[tüüp]
+            lopp=True
 
-
-
+        #if elud==0:
+            #gameplay=False
 
 #joonistame
     aken.fill((200,200,200))
-    pg.draw.rect(aken, (100,150,150), (x_player, y_player, a, a))
+    pg.draw.rect(aken, (100,150,150), (x_player, y_player, tegelane, tegelane))
 
 #tegelintski
     pg.draw.rect(aken, värv, (x_kast, y_kast, a, a))
 
 #kiri
     font = pygame.font.SysFont(None, 24)
-    txt = font.render("skoor: " + str(skoor), True, "black")
-    aken.blit(txt, (20, 20))
+    skoortekst = font.render("skoor: " + str(skoor), True, "black")
+    aken.blit(skoortekst, (20, 20))
+    font = pygame.font.SysFont(None, 24)
+    elutekst = font.render("elu: " + str(elud), True, "black")
+    aken.blit(elutekst, (20, 50))
     pg.display.update()
+
+    if lopp== True:
+        tüüp = choice(list(erinevad_kastid.keys()))
+        värv = erinevad_kastid[tüüp]
+        lopp=False
+
 
 
 
